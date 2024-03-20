5be5920ff269e8c77a52ff69dbbe22aef8352446
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-03-20 18:52:22 -0300

    conflict

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 /// <reference path="../.astro/db-types.d.ts" />
->>>>>>> 2f08b1d (feat/db)
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
