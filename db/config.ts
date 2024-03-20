a913c142c826d524fb0688deec125e08110c87e8
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-03-20 18:37:50 -0300

    db

--- db/config.ts
+++ db/config.ts
@@ -1,21 +1,14 @@
 import { column, defineDb, defineTable } from 'astro:db'
 
-const Artist = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true }),
-		name: column.text(),
-	},
-})
-
 const Lylic = defineTable({
 	columns: {
 		id: column.number({ primaryKey: true }),
-		text: column.text(),
-		artistId: column.number({ references: () => Artist.columns.id }),
+		quote: column.text(),
+		artist: column.text(),
 	},
 })
 
 // https://astro.build/db/config
 export default defineDb({
-	tables: {},
+	tables: { Lylic },
 })
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,16 @@
+import type { APIRoute } from 'astro'
+import { db, Lylic } from 'astro:db'
 // GET /
-export async function GET() {
-  console.log('Hello World');
-  return new Response(JSON.stringify({ message: 'Hello World' }));
+export const GET: APIRoute = async () => {
+	const lylics = await db.select().from(Lylic).all()
+	console.log({ lylics })
+	return new Response(JSON.stringify(lylics))
+}
+
+export const POST: APIRoute = async ({ request }) => {
+	const body = await request.json()
+	console.log({ body })
+	const { quote, artist } = body
+	const lylic = await db.insert(Lylic).values({ quote, artist }).returning()
+	return new Response(JSON.stringify(lylic))
 }
