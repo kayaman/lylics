e0b05c0842da9641402685080dfa6c17ba8e36c6
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-03-21 03:51:38 -0300

    Revert "Docs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
+
 import vercel from "@astrojs/vercel/serverless";
-import openapiBackend from "@astro-openapi/backend";
-import openapiTypegen from "@astro-openapi/typegen";
-import openapiBundler from "@astro-openapi/bundler";
-import openapiClient from "@astro-openapi/client";
-import openapiGuiSwagger from "@astro-openapi/gui-swagger";
-import openapiGuiRedoc from "@astro-openapi/gui-redoc";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [db(), openapiBackend(), openapiTypegen(), openapiBundler(), openapiClient(), openapiGuiSwagger(), openapiGuiRedoc()],
+  integrations: [db()],
   adapter: vercel()
 });
